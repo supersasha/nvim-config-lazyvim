8918b4acf9c6caab2bcab5339f15132c5eb4bd0e
supersasha <supersasha.ru@gmail.com>
2025-03-13 14:56:34 +0400

    Disabled inlay hints

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -9,12 +9,12 @@ return {
   --      return {}
   --    end,
   --  },
-  --  {
-  --    "neovim/nvim-lspconfig",
-  --    opts = {
-  --      inlay_hints = {
-  --        enabled = false,
-  --      },
-  --    },
-  --  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+    },
+  },
 }
